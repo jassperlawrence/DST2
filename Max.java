cc4851ef388b03ff77dbb2a1f4704376fba57188
jassperlawrence <54319817+jassperlawrence@users.noreply.github.com>
2020-03-05 12:40:09 +0800

    Update Max.java

--- Max.java
+++ Max.java
@@ -11,4 +11,22 @@ package dst2;
  */
 public class Max {
     
+    public Max() {
+    }
+    
+    public static  <T extends Comparable<T>> T max(T a,T b,T c){
+        T max=a;
+        if(b.compareTo(max)>0){
+            max=b;
+        }
+        else if(c.compareTo(max)>0){
+            max=c;
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        Max a = new Max();
+        System.out.println( a.max(30.0,20.1,50.4)); 
+    }
+    
 }
